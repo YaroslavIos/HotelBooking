b1da99558fe476c977ff2417a8decb3379608591
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-12-20 22:40:02 +0300

    Initial Commit

---
+++ HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by Ярослав Любиченко on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
